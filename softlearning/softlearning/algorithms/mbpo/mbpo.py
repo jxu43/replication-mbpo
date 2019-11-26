9c25910383b6a01e9c072f82275df420863e0e07
Wang Siyao <1521925222@qq.com>
2019-11-25 21:07:44 -0500

    Train Policy Reshape

--- softlearning/softlearning/algorithms/mbpo/mbpo.py
+++ softlearning/softlearning/algorithms/mbpo/mbpo.py
@@ -176,8 +176,8 @@ def train_policy_repeats(args, total_step, train_step, cur_step, env_pool, model
         if model_batch_size > 0:
             model_state, model_action, model_reward, model_next_state, model_done = model_pool.sample(int(model_batch_size))
             batch_state, batch_action, batch_reward, batch_next_state, batch_done = np.concatenate((env_state, model_state), axis=0), \
-                np.concatenate((env_action, model_action), axis=0), np.concatenate((env_reward, model_reward), axis=0), \
-                np.concatenate((env_next_state, model_next_state), axis=0), np.concatenate((env_done, model_done), axis=0)
+                np.concatenate((env_action, model_action), axis=0), np.concatenate((np.reshape(env_reward, (env_reward.shape[0], -1)), model_reward), axis=0), \
+                np.concatenate((env_next_state, model_next_state), axis=0), np.concatenate((np.reshape(env_done, (env_done.shape[0], -1)), model_done), axis=0)
         else:
             batch_state, batch_action, batch_reward, batch_next_state, batch_done = env_state, env_action, env_reward, env_next_state, env_done
 
