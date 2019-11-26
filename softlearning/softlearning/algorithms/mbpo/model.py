7b87ff63fa3bfc345cdf8b82faf451c9887d06d1
Wang Siyao <1521925222@qq.com>
2019-11-25 19:45:31 -0500

    Debug + Tensor Shape

--- softlearning/softlearning/algorithms/mbpo/model.py
+++ softlearning/softlearning/algorithms/mbpo/model.py
@@ -55,9 +55,9 @@ class Game_model(nn.Module):
         nn4_output = self.nn4(nn3_output)
         nn5_output = self.nn5(nn4_output)
 
-        mean = nn5_output[:, :self.output_dim + 1]
+        mean = nn5_output[:, :self.output_dim]
 
-        logvar = self.max_logvar - torch.nn.Softplus(self.max_logvar - nn5_output[:, self.output_dim + 1:])
+        logvar = self.max_logvar - torch.nn.Softplus(self.max_logvar - nn5_output[:, self.output_dim:])
         logvar = self.min_logvar + torch.nn.Softplus(logvar - self.min_logvar)
 
         return mean, logvar
