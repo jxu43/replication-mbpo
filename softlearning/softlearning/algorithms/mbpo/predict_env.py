ec8d03a6f5ebdbcca62f34da2f47ee7862766604
Wang Siyao <1521925222@qq.com>
2019-11-26 22:24:39 -0500

    Walker2d

--- softlearning/softlearning/algorithms/mbpo/predict_env.py
+++ softlearning/softlearning/algorithms/mbpo/predict_env.py
@@ -19,6 +19,18 @@ class PredictEnv:
             done = ~not_done
             done = done[:,None]
             return done
+        else if env_name == "Walker2d-v2":
+            assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
+
+            height = next_obs[:, 0]
+            angle = next_obs[:, 1]
+            not_done =  (height > 0.8) \
+                        * (height < 2.0) \
+                        * (angle > -1.0) \
+                        * (angle < 1.0)
+            done = ~not_done
+            done = done[:,None]
+            return done
 
     def _get_logprob(self, x, means, variances):
 
